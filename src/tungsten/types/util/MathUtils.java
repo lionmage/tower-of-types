5b9067165303356068bc9e89db01b522f53c041c
Robert Poole <Tarquin.AZ@gmail.com>
2018-03-17 14:02:24 -0700

    Added caching to factorial calculation for added efficiency.

--- src/tungsten/types/util/MathUtils.java
+++ src/tungsten/types/util/MathUtils.java
@@ -24,6 +24,10 @@
 package tungsten.types.util;
 
 import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import tungsten.types.numerics.IntegerType;
 import tungsten.types.numerics.impl.IntegerImpl;
 
@@ -35,17 +39,53 @@ import tungsten.types.numerics.impl.IntegerImpl;
 public class MathUtils {
     private static final BigInteger TWO = BigInteger.valueOf(2L);
     
+    private static final Map<Long, BigInteger> factorialCache = new HashMap<>();
+    
     public static IntegerType factorial(IntegerType n) {
         if (n.asBigInteger().equals(BigInteger.ZERO) || n.asBigInteger().equals(BigInteger.ONE)) {
             return new IntegerImpl(BigInteger.ONE);
+        } else if (getCacheFor(n) != null) {
+            return new IntegerImpl(getCacheFor(n));
         }
         
-        BigInteger accum = BigInteger.ONE;
+        final long ncmp = n.asBigInteger().longValueExact();
+        Long m = factorialCache.keySet().parallelStream().filter(x -> x < ncmp).max(Long::compareTo).orElse(null);
+        
+        BigInteger accum = m != null ? factorialCache.get(m) : BigInteger.ONE;
         BigInteger intermediate = n.asBigInteger();
-        while (intermediate.compareTo(TWO) >= 0) {
+        BigInteger bailout = m != null ? BigInteger.valueOf(m + 1L) : TWO;
+        while (intermediate.compareTo(bailout) >= 0) {
             accum = accum.multiply(intermediate);
             intermediate = intermediate.subtract(BigInteger.ONE);
         }
+        cacheFact(n, accum);
         return new IntegerImpl(accum);
+    }
+    
+    private static final BigInteger MAX_LONG = BigInteger.valueOf(Long.MAX_VALUE);
+    
+    private static void cacheFact(BigInteger n, BigInteger value) {
+        if (n.compareTo(TWO) >= 0 && n.compareTo(MAX_LONG) < 0) {
+            Long key = n.longValueExact();
+            
+            if (!factorialCache.containsKey(key)) factorialCache.put(key, value);
+        }
+    }
+    
+    private static void cacheFact(IntegerType n, BigInteger value) {
+        cacheFact(n.asBigInteger(), value);
+    }
+    
+    private static BigInteger getCacheFor(BigInteger n) {
+        try {
+            return factorialCache.get(n.longValueExact());
+        } catch (ArithmeticException e) {
+            Logger.getLogger(MathUtils.class.getName()).log(Level.FINER, "Attempt to cache a factorial value for n outside Long range.", e);
+            return null; // this is the same as if we had a regular cache miss
+        }
+    }
+    
+    private static BigInteger getCacheFor(IntegerType n) {
+        return getCacheFor(n.asBigInteger());
     }
 }
